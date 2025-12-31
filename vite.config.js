16ad5c81130dce9bd662db10692b88a8aa600ad9
hari haran <142453359+hari-haran80@users.noreply.github.com>
2025-12-31 19:01:40 +0530

    fixed error

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
 });
